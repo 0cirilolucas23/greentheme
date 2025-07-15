ea797b362b998dc60e5c932364c410346347b8a1
0cirilolucas23 <lucas.cirilogomes@gmail.com>
2025-07-15 20:27:16 -0300

    Feat: Adc do template-full-width para o elementor ter controle de largura total

---
+++ template-full-width.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: Elementor Full Width
+Template Post Type: page, post
+*/
+get_header(); ?>
+
+<main>
+    <?php
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <?php the_content(); ?>
+            </article>
+            <?php
+        endwhile;
+    endif;
+    ?>
+</main>
+
+<?php get_footer(); ?>
